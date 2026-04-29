2df8b64e79df2fe3e07120dc04089ecb43a339af
a-bad-dev <244852891+a-bad-dev@users.noreply.github.com>
2026-04-28 21:57:50 -0300

    Update scripts for 5.16.0-rc1

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # run this script on an aarch64 computer, running modern debian or
 # something similar, otherwise it's not going to work...
 
-VERSION="5.15.2"
+VERSION="5.16.0-rc1"
 SDL_VERSION="2.32.10"
 
 BOLD="\x1b[1m"
